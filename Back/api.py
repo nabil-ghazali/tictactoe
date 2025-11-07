f860d1f745c4cd83bc9748661ad64173562e275e
GaetanCSimplon <gcourtieupro@gmail.com>
2025-11-07 10:12:59 +0100

    Réglage dans model.py -> Timeout=None car réponse du modèle lente (en local)

--- Back/api.py
+++ Back/api.py
@@ -1,43 +1,12 @@
 from typing import List, Dict
-import json
-from Back.game_logic import format_grid_for_llm
+from .game_logic import format_grid_for_llm
 from Model.model import LLMClient
+from .move_request import MoveRequest
 from fastapi import FastAPI, HTTPException
 from starlette.middleware.cors import CORSMiddleware
-from pydantic import BaseModel, field_validator, ValidationError
-from typing import List, Dict
 import httpx
 
-app = FastAPI()
 
-class MoveRequest(BaseModel):
-    grid: List[List[int]]
-    active_player_id: int
-    model_name: str
-    
-    # Validation du format des données envoyées
-    @field_validator('grid')
-    @classmethod
-    def validate_grid_dimensons(cls, v: List[List[int]]):
-        GRID_SIZE = 10 # Définie la taille attendue (10x10)
-        # Vérifie si le nombre de lignes envoyé correspond au GRID_SIZE
-        if len(v) != GRID_SIZE:
-            raise ValueError(f"La grille doit avoir {GRID_SIZE} lignes. Reçu: {len(v)}")
-        # Vérifie si chaque ligne a la bonne taille (nombre de colonne = 10)
-        for row in v:
-            if len(row) != GRID_SIZE:
-                raise ValueError(f"Toutes les lignes de la grille doivent avoir {GRID_SIZE} colonnes.")
-        return v
-    
-    @field_validator('active_player_id')
-    @classmethod
-    def validate_player_id(cls, v: int):
-        # Le joueur doit 1 (X) ou 2 (O)
-        ALLOWED_PLAYER = {1, 2}
-        
-        if v not in ALLOWED_PLAYER:
-            raise ValueError(f"L'ID du joueur actif doit 1 (X) ou 2 (o). Reçu: {v}")
-        return v
 
 app = FastAPI()
 
@@ -66,5 +35,6 @@ async def play(request: MoveRequest):
     except HTTPException as e:
         raise e
     except Exception as e:
+        print(f"Erreur interne non gérée: {type(e).__name__}: {e}")
         raise HTTPException(status_code=500, detail=f"Erreur interne lors du traitement du coup: {e}")
 
